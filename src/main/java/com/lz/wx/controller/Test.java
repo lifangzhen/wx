4f83d402e194743df98abff96a74b388d7274a2a
ybl <2694972473@qq.com>
2018-07-20 22:42:08 +0800

    aa

--- src/main/java/com/lz/wx/controller/Test.java
+++ src/main/java/com/lz/wx/controller/Test.java
@@ -4,14 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by 71426 on 2018/7/19.
  */
 @Controller
 public class Test {
     @RequestMapping("/index.htm")
-    public ModelAndView index() {  
+    public ModelAndView index() {
         ModelAndView mav = new ModelAndView("/templates/index.ftl");
+        String aa = "fdsafdsa";
         return mav;
     }
 }
