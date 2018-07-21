e1fc00c6e6e744291765fd1fdc131364da062fe6
ybl <2694972473@qq.com>
2018-07-21 21:49:27 +0800

    boo

---
+++ src/main/java/com/lz/wx/controller/boo.java
@@ -0,0 +1,14 @@
+package com.lz.wx.controller;
+
+/**
+ * Created by 26949 on 2018/7/21.
+ */
+public class boo {
+    public static void main(String[] args) {
+        int lang=2;
+        int yang=6;
+       // lang=lang+7;
+        boolean pk=(lang=lang+2)>yang;
+        System.out.println(pk);
+    }
+}
