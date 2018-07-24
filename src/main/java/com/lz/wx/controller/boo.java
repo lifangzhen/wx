6241a12eecb5e8f8fd07fde0f691d19929c987b6
ybl <2694972473@qq.com>
2018-07-25 00:35:29 +0800

    shenli

--- src/main/java/com/lz/wx/controller/boo.java
+++ src/main/java/com/lz/wx/controller/boo.java
@@ -3,21 +3,17 @@ package com.lz.wx.controller;
 /**
  * Created by 26949 on 2018/7/21.
  */
+        /*三目运算*/
 public class boo {
     public static void main(String[] args) {
-        int x=8;
-       // x+=5;//相当于x=x+5
-        x*=5;//相当于x=x*5
-
-
-        System.out.println(x);
-
-
-
-
-
-
-
+       char ts='人';
+       char bgj='妖';
+        char ry='变';
+        char Who=ts;
+        char r=Who;
+       Who=bgj;
+        r=Who=='人'?'留': Who=='妖'?'杀':'泰';
+       System.out.println(r);
 
 
 
---
+++ src/main/java/com/lz/wx/controller/day01.java
@@ -0,0 +1,7 @@
+package com.lz.wx.controller;
+
+/**
+ * Created by 26949 on 2018/7/24.
+ */
+public class day01 {
+}
