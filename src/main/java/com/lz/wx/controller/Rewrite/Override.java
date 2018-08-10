d8cfe4a9ee624c64caeac805da09fd865fa981a7
ybl <2694972473@qq.com>
2018-08-10 18:51:59 +0800

    rewrite

--- src/main/java/com/lz/wx/controller/Rewrite/Override.java
+++ src/main/java/com/lz/wx/controller/Rewrite/Override.java
@@ -16,6 +16,21 @@ public class Override {
         c1.pay(money,fee);
         //小儿子做法
         c2.pay(money,fee);
+
+        /*动态方法调度 */
+
+        paernt dad=null;//定义一个父类型的变量引用
+        Child1 sin1=new Child1();
+        Child2 sin2=new Child2();
+        dad=sin1;//爸爸和大儿子一起做蛋炒饭
+        dad.cook();
+        dad=sin2;//爸爸和小儿子一起包饺子
+        dad.cook();
+        dad=new paernt();//只有父类型对象自己才能调用父类自己的方法
+        dad.cook();
+
+
+
     }
 }
 /*交学费的方法
@@ -23,6 +38,10 @@ public class Override {
 *   fee 实际学费
 * */
 class paernt{
+    /* 爸爸做饭的任务*/
+    public void cook(){
+        System.out.println("爸爸的任务是：做饭");
+    }
     public void pay(double money,double fee){
         System.out.println("家长让交"+fee+"元学费，剩下的"+(money-fee)+"零花钱");
     }
@@ -31,6 +50,10 @@ class paernt{
 * 封装大儿子子类
 * */
 class Child1 extends paernt{
+    /*大儿子任务卡是：蛋炒饭 */
+    public void cook(){
+        System.out.println("帮大儿子做蛋炒饭");
+    }
    public void pay(double money,double fee){
        System.out.println("没交学费买了"+money+"元变形金刚");
    }
@@ -39,6 +62,10 @@ class Child1 extends paernt{
 * 封装小儿子的类
 * */
 class Child2 extends paernt{
+    /*小儿子的任务卡是：包饺子*/
+    public void cook(){
+        System.out.println("帮小儿子包饺子");
+    }
     public void pay(double money,double fee){
         //按照要求先交学费
         super.pay(money,fee);//使用super关键字，调用父类的方法
