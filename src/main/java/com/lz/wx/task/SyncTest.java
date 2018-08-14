7bee11fdbed2f965360f13eae39e013305a2889a
policy <lifangzhen@juniuo.com>
2018-08-14 16:46:56 +0800

    sync

---
+++ src/main/java/com/lz/wx/task/SyncTest.java
@@ -0,0 +1,34 @@
+package com.lz.wx.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by 71426 on 2018/8/7.
+ */
+@Component
+@EnableAsync
+public class SyncTest {
+    @Autowired private UserService userService;
+
+    @Async
+    @Scheduled(fixedDelay = 1)
+    public void test1() throws InterruptedException {
+        for (int i=0;i<10;i++){
+            String userId = i+"id";
+            userService.getUser(userId);
+        }
+    }
+
+    @Async
+    @Scheduled(fixedDelay = 1)
+    public void test2() throws InterruptedException {
+        for (int i=0;i<10;i++){
+            String userId = i+"id";
+            userService.getUser(userId);
+        }
+    }
+}
---
+++ src/main/java/com/lz/wx/task/UserService.java
@@ -0,0 +1,15 @@
+package com.lz.wx.task;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by 71426 on 2018/8/7.
+ */
+@Service
+public class UserService {
+
+    public void getUser(String userId){
+
+        System.out.println(userId);
+    }
+}
