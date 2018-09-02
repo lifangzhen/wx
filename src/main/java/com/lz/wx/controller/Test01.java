8be60dee3c5320dabd7e57cdd0652935ccd74500
ybl <2694972473@qq.com>
2018-09-02 21:13:49 +0800

    test01

---
+++ src/main/java/com/lz/wx/controller/Test01.java
@@ -0,0 +1,26 @@
+package com.lz.wx.controller;
+
+/**
+ * Created by 26949 on 2018/9/2.
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        //创建学生对象
+        Student student=new Student();
+        student.name="王以太";
+        student.age=18;
+        student.course="java";
+        student.hobby="唱歌";
+        student.show();
+
+
+
+        Teacher teacher=new Teacher();
+        teacher.name="王硕";
+        teacher.major="java";
+        teacher.giveCourse="java后端开发";
+        teacher.teacingAge=10;
+        teacher.show();
+    }
+}
+ 
