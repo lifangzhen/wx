deac8d5ef06118b3151b1fb7e20f57068d589539
policy <lifangzhen@juniuo.com>
2018-09-03 19:28:23 +0800

    test

--- src/main/java/com/lz/wx/task/SyncTest.java
+++ src/main/java/com/lz/wx/task/SyncTest.java
@@ -1,5 +1,6 @@
 package com.lz.wx.task;
 
+import com.lz.wx.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -14,14 +15,18 @@ import org.springframework.stereotype.Component;
 public class SyncTest {
     @Autowired private UserService userService;
 
-    @Async
-//    @Scheduled(fixedDelay = 1)
-    public void test1() throws InterruptedException {
-//        for (int i=0;i<10;i++){
-//            String userId = i+"id";
-//            userService.getUser(userId);
+//    @Async
+//    @Scheduled(fixedDelay = 5)
+//    public void test1() throws InterruptedException {
+//        for (int i=0;i<1000;i++){
+//            String name = "name"+i;
+//            String mobile = (1590102000+i)+"";
+//            String password = "123456";
+//            Integer age = i==0?0:1000%i;
+//            Integer sex = 1;
+//            userService.regist(name, mobile, password, age, sex);
 //        }
-    }
+//    }
 
     @Async
 //    @Scheduled(fixedDelay = 1)
